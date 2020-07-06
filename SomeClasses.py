bb65ae92c1cf14605e79de52b190d7c5413e6050
Justin Skycak <jpskycak@gmail.com>
2020-07-06 22:21:14 +0000

    Initial commit

---
+++ SomeClasses.py
@@ -0,0 +1,7 @@
+
+class CoolClass():
+  def __init__(self, val):
+    self.val = val
+
+  def cool_function(self):
+    return self.val
---
+++ main.py
@@ -0,0 +1,4 @@
+from SomeClasses import CoolClass
+
+cool_class = CoolClass(5)
+print(cool_class.cool_function())
